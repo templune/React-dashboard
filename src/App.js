482e88ae7c478ab1f2553fcd5f80f43384b3d058
templune <templune@gmail.com>
2021-07-06 22:37:54 -0500

    Se instalo reactstrap y se anadio el navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route} from 'react-router-dom' 
-import Navbar from '../src/components/Navbar'
+import NavbarNav from '../src/components/NavbarNav'
 import Sidebar from '../src/components/Sidebar'
 import Home from '../src/pages/Home'
 import Sales from '../src/pages/Sales'
@@ -9,10 +9,10 @@ import './App.scss';
 function App() {
   return (
     <Router>
-      <Navbar />
       <div className="flex">
         <Sidebar />
-        <div className="content">
+        <div className="content w-100">
+          <NavbarNav />
           <Route path="/" exact={true} component={Home} />
           <Route path="/sales" exact={true} component={Sales} />
           <Route path="/clients" exact={true} component={Clients} />
--- src/components/Navbar.js
+++
@@ -1,9 +0,0 @@
-const Navbar = () => {
-    return (
-        <div className="navbar">
-            Navbar
-        </div>
-    )
-}
-
-export default Navbar
